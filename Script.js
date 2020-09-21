cee93d588f075dd1a0086119e4373ddccde55bd2
Jason Aultman <jason.aultman@gmail.com>
2020-09-21 15:44:32 -0400

    changed name of function added additional function

--- Script.js
+++ Script.js
@@ -1,12 +1,17 @@
 
 const allButtons = document.getElementById('inputButton');
 const calcDisplay = document.getElementById('output');
-function buttonClickEvent(number){
+function buttonClickEventClickNumber(number){
     if((calcDisplay.innerHTML).trim()==='0'){
         calcDisplay.innerHTML='';
 }
 let holder = calcDisplay.innerHTML;
 calcDisplay.innerHTML=holder+number;
 };    
+
+function buttonClickEventClickOperator(operand)
+{
+    
+}
 
 
