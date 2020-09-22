a1ec233cad787d69092da2da86296bb2a820088e
Jason Aultman <jason.aultman@gmail.com>
2020-09-21 22:00:52 -0400

    final project

--- Script.js
+++ Script.js
@@ -1,6 +1,10 @@
 
 const allButtons = document.getElementById('inputButton');
 const calcDisplay = document.getElementById('output');
+var operand;
+var placeholderOne;
+var placeholderTwo;
+
 function buttonClickEventClickNumber(number){
     if((calcDisplay.innerHTML).trim()==='0'){
         calcDisplay.innerHTML='';
@@ -9,9 +13,33 @@ let holder = calcDisplay.innerHTML;
 calcDisplay.innerHTML=holder+number;
 };    
 
-function buttonClickEventClickOperator(operand)
+function buttonClickEventClickOperand(operand)
 {
     
-}
+    this.operand=operand;
+    if(placeholderOne!==undefined)
+    {
+        placeholderTwo=calcDisplay.innerHTML;
+    }else placeholderOne = calcDisplay.innerHTML;
+    placeholder = calcDisplay.innerHTML;
+    calcDisplay.innerHTML='0';
+    
+};
+
+function buttonClickEventEquals(){
+    placeholderTwo=calcDisplay.innerHTML;
+    if(operand==='+'){
+        var result = parseFloat(placeholderOne)+parseFloat(placeholderTwo);
+    }else if(operand==='-'){
+        var result = parseFloat(placeholderOne)-parseFloat(placeholderTwo);
+    }else if (operand ==='X'){
+        var result = parseFloat(placeholderOne)*parseFloat(placeholderTwo);
+    }else if (operand==='/'){
+        var result = parseFloat(placeholderOne)/parseFloat(placeholderTwo);
+    }
+
+    result=result.toString();
+    calcDisplay.innerHTML=result;
+};
 
 
